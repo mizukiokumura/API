924c4796d16fa4b5bce2d77fe9d61dd226f63dea
mizukiokumura <nihonnpart4@yahoo.co.jp>
2021-09-17 11:42:52 +0900

    Initial commit

---
+++ Account.java
@@ -0,0 +1,23 @@
+public class Account {
+  int balance;
+  String accountNumber;
+  public String toString() {
+    return "¥¥" + this.balance + "(口座番号:" + this.accountNumber + ")";
+
+  }
+
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o instanceof Account) {
+      Account a = (Account)o
+      String an1 = this.accountNumber.trim();
+      String an2 = a.accountNumber.trinm();
+      if (an1.equals(an2)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
